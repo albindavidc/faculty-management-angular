292db9fc3e9a980916e5acdc2ec4fcd629a0b502
Albin David C <albindavidc007@gmail.com>
2025-01-27 12:43:05 +0530

    updated sidebar in the frontend

--- faculty-management/src/app/app.routes.ts
+++ faculty-management/src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './admin/components/login/login.component';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
-    children: [{ path: 'login', component: LoginComponent }],
+    children: [
+      { path: 'login', component: LoginComponent },
+      { path: 'admin-dashboard', component: AdminDashboardComponent },
+    ],
   },
   {
     path: '',
