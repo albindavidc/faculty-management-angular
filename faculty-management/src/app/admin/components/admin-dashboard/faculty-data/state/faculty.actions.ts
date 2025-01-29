e4e3bb0f76518709527e9a2c7b476a304292afae
Albin David C <albindavidc007@gmail.com>
2025-01-29 11:22:45 +0530

    created action for adding and loading faculties

--- faculty-management/src/app/admin/components/admin-dashboard/faculty-data/state/faculty.actions.ts
+++ faculty-management/src/app/admin/components/admin-dashboard/faculty-data/state/faculty.actions.ts
@@ -1,10 +1,15 @@
 import { createActionGroup, emptyProps, props } from '@ngrx/store';
+import { Faculty } from '../../../../../model/faculty';
 
 export const FacultyActions = createActionGroup({
   source: 'Faculty',
   events: {
-    'Faculty Facultys': emptyProps(),
-    'Faculty Facultys Success': props<{ data: unknown }>(),
-    'Faculty Facultys Failure': props<{ error: unknown }>(),
-  }
+    'Add Faculty': props<{ faculty: Faculty }>(),
+    'Add Faculty Success': props<{ faculty: Faculty }>,
+    'Add Faculty Failure': props<{ error: any }>,
+
+    'Load Faculty': emptyProps(),
+    'Load Faculties Success': props<{ faculties: Faculty[] }>,
+    'Load Faculties Failure': props<{ error: any }>,
+  },
 });
