612b8557a7fe2e520d45c83010fafe9cf69eef64
Albin David C <albindavidc007@gmail.com>
2025-02-02 07:25:42 +0530

    created admin signup page

--- faculty-management/src/app/admin/admin.component.ts
+++ faculty-management/src/app/admin/admin.component.ts
@@ -3,10 +3,11 @@ import { RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { AdminAuthService } from './service/admin-auth.service';
 import { CommonModule } from '@angular/common';
+import { AdminSignupComponent } from './components/admin-signup/admin-signup.component';
 
 @Component({
   selector: 'app-admin',
-  imports: [RouterModule, LoginComponent, CommonModule],
+  imports: [RouterModule, AdminSignupComponent, CommonModule],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css',
 })
--- faculty-management/src/app/app.routes.ts
+++ faculty-management/src/app/app.routes.ts
@@ -3,13 +3,14 @@ import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './admin/components/login/login.component';
 import { AdminDashboardComponent } from './admin/components/admin-dashboard/admin-dashboard.component';
 import { adminGuard } from './service/admin.guard';
+import { AdminSignupComponent } from './admin/components/admin-signup/admin-signup.component';
 
 export const routes: Routes = [
   {
     path: 'admin',
     component: AdminComponent,
     children: [
-      { path: 'login', component: LoginComponent },
+      { path: 'login', component: AdminSignupComponent },
       {
         path: 'admin-dashboard',
         component: AdminDashboardComponent,
